35fb7f31b7bd1b59e1ccde21748b30362a34d9f5
Ziwei <zwyin2013@berkeley.edu>
2016-11-09 20:13:59 -0800

    Still that weird bug

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -9,14 +9,31 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    var w: CGFloat!
+    var h: CGFloat!
+    var numGuessed = 0
+    var letterGuessed = [String]()
+    var phrase = ""
+    var incorrectGuess = [String]()
+
+    @IBOutlet weak var reminder: UILabel!
+    @IBOutlet weak var guessLabel: UILabel!
+    @IBOutlet weak var hangman: UIImageView!
+    @IBOutlet weak var guess: UITextField!
+    @IBOutlet weak var guessButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        w = self.view.bounds.size.width
+        h = self.view.bounds.size.height
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        let phrase = hangmanPhrases.getRandomPhrase()
+        phrase = hangmanPhrases.getRandomPhrase()
         print(phrase)
+        makephrase()
+        reminder.text = "Please enter your guess. One letter only."
+        updateImage()
+        updateGuessedLabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,15 +41,100 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func updateImage() {
+        let name = "hangman" + String(numGuessed + 1) + ".gif"
+        let myImage = UIImage(named: name)
+        hangman.image = myImage
     }
-    */
+    
+    func makephrase() {
+        let p = phrase
+        var index = 0
+        let letterwidth = (w - 15.0) / CGFloat(p.characters.count)
+        let letterheight = h / 9
+        for c in p.characters {
+            let v = UILabel(frame: CGRect(x: CGFloat(index) * letterwidth + 15.0, y: h * (4/9), width: letterwidth, height: letterheight))
+            if letterGuessed.contains(String(c)) {
+                v.text = String(c)
+            }
+            else if c != " " {
+                v.text = String("_")
+            }
+            v.adjustsFontSizeToFitWidth = true
+            self.view.addSubview(v)
+            index += 1
+        }
+    }
 
+    func updateGuessedLabel() {
+        var text = "Incorrect Guesses:"
+        for c in incorrectGuess {
+            text = text + " " + String(c) + " "
+        }
+        guessLabel.text = text
+    }
+    
+    @IBAction func pressedGuess(_ sender: Any) {
+        let yourguess = guess.text!.uppercased()
+        guess.text = ""
+        if yourguess.characters.count != 1 {
+            reminder.text = "Please enter one letter only."
+        }
+        else if letterGuessed.contains(yourguess) {
+            reminder.text = "You already tried this letter."
+        }
+        else {
+            if phrase.characters.contains(Character(yourguess)) {
+                letterGuessed += [yourguess]
+                makephrase()
+                reminder.text = "Great Guess"
+                checkwin()
+            }
+            else {
+                numGuessed += 1
+                letterGuessed += [yourguess]
+                incorrectGuess += [yourguess]
+                reminder.text = "Bad Guess"
+                checklose()
+                updateGuessedLabel()
+                updateImage()
+            }
+        }
+    }
+
+    func final(alert: UIAlertAction) {
+        let hangmanPhrases = HangmanPhrases()
+        phrase = hangmanPhrases.getRandomPhrase()
+        letterGuessed = [String]()
+        incorrectGuess = [String]()
+        numGuessed = 0
+        reminder.text = "Please enter your guess. One letter only."
+        updateImage()
+        updateGuessedLabel()
+        makephrase()
+    }
+    
+    func checklose() {
+        if numGuessed == 7 {
+            let lose = UIAlertController(title: "You Lose", message: "It's ok! Do you want to start another game?", preferredStyle: .alert)
+            let firstAction = UIAlertAction(title: "Startover", style: .default, handler: final)
+            lose.addAction(firstAction)
+            self.present(lose, animated: true, completion: nil)
+        }
+    }
+
+    func checkwin() {
+        var win = true
+        for c in phrase.characters {
+            if letterGuessed.contains(String(c)) == false {
+                win = false
+            }
+        }
+        if win {
+            let win = UIAlertController(title: "You Win", message: "Congratulations! Do you want to start another game?", preferredStyle: UIAlertControllerStyle.alert)
+            let firstAction = UIAlertAction(title: "Startover", style: .default, handler: final)
+            win.addAction(firstAction)
+            self.present(win, animated: true, completion: nil)
+        }
+    }
 }
--- Hangman/Hangman/StartScreenViewController.swift
+++ Hangman/Hangman/StartScreenViewController.swift
@@ -12,7 +12,6 @@ class StartScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
